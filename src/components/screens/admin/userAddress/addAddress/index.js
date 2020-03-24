f53c3e4a86308ba2d0847d03549828cda86bab34
atikrantjn <atikrant.jain@neosofttech.com>
2020-03-24 19:42:44 +0530

    added some validations in add address module

--- src/components/screens/admin/userAddress/addAddress/index.js
+++ src/components/screens/admin/userAddress/addAddress/index.js
@@ -5,12 +5,15 @@ import {
   ScrollView,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 import styles from './styles';
 
 class AddAddress extends Component {
   constructor(props) {
     super(props);
+
+    this.myTextInput = React.createRef();
 
     this.state = {
       address: '',
@@ -19,9 +22,40 @@ class AddAddress extends Component {
       state: '',
       zipCode: '',
       country: '',
+
+      zipCodeErr: false,
+      allFieldsRequired: false,
     };
   }
+
+  validatezipCode = zipCode => {
+    if (zipCode.length < 6) {
+      this.setState({zipCodeErr: true});
+      return false;
+    }
+    this.setState({zipCode: zipCode});
+  };
+
+  saveAddress = () => {
+    const {address, landmark, city, state, zipCode, country} = this.state;
+
+    if (
+      address === '' ||
+      landmark === '' ||
+      city === '' ||
+      state === '' ||
+      zipCode === '' ||
+      country === ''
+    ) {
+      Alert.alert('fields cannot be kept empty');
+    }
+    Alert.alert('submitted');
+    this.myTextInput.current.clear();
+  };
+
   render() {
+    const state = {...this.state};
+    console.log(state);
     return (
       <View style={styles.mainContainer}>
         <ScrollView>
@@ -29,27 +63,49 @@ class AddAddress extends Component {
             <View style={styles.addressContainer}>
               <Text style={styles.addressText}>Address</Text>
               <TextInput
+                ref={this.myTextInput}
                 numberOfLines={4}
                 multiline
                 textAlignVertical={'top'}
                 style={styles.addressTextInput}
+                onChangeText={address => {
+                  this.setState({address: address});
+                }}
               />
             </View>
 
             <View style={styles.landmarkContainer}>
               <Text style={styles.landmarkText}>Landmark</Text>
-              <TextInput style={styles.landmarkTextInput} />
+              <TextInput
+                ref={this.myTextInput}
+                style={styles.landmarkTextInput}
+                onChangeText={landmark => {
+                  this.setState({landmark: landmark});
+                }}
+              />
             </View>
 
             <View style={styles.cityStateContainer}>
               <View style={styles.cityTextContainer}>
                 <Text style={styles.cityText}>City</Text>
-                <TextInput style={styles.cityTextInput} />
+                <TextInput
+                  ref={this.myTextInput}
+                  style={styles.cityTextInput}
+                  onChangeText={city => {
+                    this.setState({city: city});
+                  }}
+                />
               </View>
 
               <View style={styles.stateTextContainer}>
                 <Text style={styles.stateText}>State</Text>
-                <TextInput style={styles.stateTextInput} />
+                <TextInput
+                  ref={this.myTextInput}
+                  style={styles.stateTextInput}
+                  onChangeText={state => {
+                    this.setState({state: state});
+                  }}
+                />
               </View>
             </View>
 
@@ -57,20 +113,40 @@ class AddAddress extends Component {
               <View style={styles.zipContainer}>
                 <Text style={styles.zipText}>Zip Code</Text>
                 <TextInput
+                  ref={this.myTextInput}
                   keyboardType={'number-pad'}
+                  maxLength={6}
                   style={styles.zipTextInput}
+                  onChangeText={zipCode => {
+                    this.validatezipCode(zipCode);
+                  }}
                 />
+                {this.state.zipCodeErr ? (
+                  <Text style={{fontSize: 15, color: 'red', marginLeft: 15}}>
+                    * should be 6 digits only
+                  </Text>
+                ) : null}
               </View>
 
               <View style={styles.countryContainer}>
                 <Text style={styles.countryText}>Country</Text>
-                <TextInput style={styles.countryTextInput} />
+                <TextInput
+                  ref={this.myTextInput}
+                  style={styles.countryTextInput}
+                  onChangeText={country => {
+                    this.setState({country: country});
+                  }}
+                />
               </View>
             </View>
           </View>
         </ScrollView>
-        <View style={styles.saveBtnContainer}>
-          <TouchableOpacity style={styles.saveAddressBtnBg}>
+        <View>
+          <TouchableOpacity
+            style={styles.saveAddressBtnBg}
+            onPress={() => {
+              this.saveAddress();
+            }}>
             <Text style={styles.saveAddressBtnText}>SAVE ADDRESS</Text>
           </TouchableOpacity>
         </View>
--- src/components/screens/admin/userAddress/addAddress/styles.js
+++ src/components/screens/admin/userAddress/addAddress/styles.js
@@ -77,6 +77,7 @@ const styles = StyleSheet.create({
   },
 
   saveAddressBtnBg: {
+    marginTop: 15,
     backgroundColor: 'red',
     borderRadius: 7,
     height: 60,
