885559a16a5d6e759667deaa2ba7e4e82d1d1c0c
atikrantjn <atikrant.jain@neosofttech.com>
2020-07-01 14:07:36 +0530

    centered header title in my cart screen and added burger icon for sidebar navigation

--- src/components/screens/admin/stackRoutes/myCartStack/index.js
+++ src/components/screens/admin/stackRoutes/myCartStack/index.js
@@ -2,44 +2,42 @@ import React, {Component} from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import appColors from '../../../../../utils/colors';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+// import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5';
 
 import MyCart from '../../myCart/index';
 const Stack = createStackNavigator();
 class MyCartStack extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {};
   }
   render() {
     return (
       <Stack.Navigator>
         <Stack.Screen
-          name="My Cart"
+          name="My cart"
           component={MyCart}
           options={{
             headerStyle: {
               backgroundColor: appColors.themeColor,
             },
             headerLeft: () => (
-              <AntDesign
-                name="back"
+              <FontAwesomeIcon
+                name="bars"
                 size={28}
                 color="white"
                 style={{marginLeft: 10}}
                 onPress={() => {
-                  this.props.navigation.navigate('Home');
+                  this.props.navigation.openDrawer();
                 }}
               />
             ),
+            headerTitleAlign: 'center',
 
             headerTitleStyle: {
               color: 'white',
               fontWeight: 'bold',
-
-              fontSize: 30,
-              marginHorizontal: 85,
+              fontSize: 28,
             },
           }}
         />
